b5d2756a38ccdfa815747c6f7082db8d8c022680
angus <tsang1205@qq.com>
2022-02-13 19:13:32 +0800

    feat: npm 包测试

--- src/utils/wxSdk.ts
+++ src/utils/wxSdk.ts
@@ -1,30 +1,19 @@
-import loadScript from './dom';
+import loadScript from 'awesome-load-script';
 
-export function loadWxScript(): Promise<typeof window.wx> {
-  return new Promise((re) => {
-    if (window.wx) {
-      re(window.wx);
-    } else {
-      console.log('init');
-      loadScript('https://res2.wx.qq.com/open/js/jweixin-1.6.0.js', {
+export const loadWxScript = () =>
+  loadScript('https://res2.wx.qq.com/open/js/jweixin-1.6.0.js', {
+    noRepeat: true,
+    autoRemove: false,
+    throwError: true,
+    exposeGlobalName: 'wx',
+    cleanGlobal: true,
+  })
+    .catch((error) => {
+      console.log(error);
+      return loadScript('/js/jweixin-1.6.0.js?', {
         noRepeat: true,
-        autoRemove: false,
-        throwError: true,
-        exposeGlobalName: 'wx',
-        cleanGlobal: true,
-      })
-        .catch((error) => {
-          console.log('error');
-          console.log(error);
-          return loadScript('/3rd/jweixin-1.4.0.js?_age=999999', {
-            noRepeat: true,
-          });
-        })
-        .then((res) => {
-          console.log(res);
-          console.log('1');
-          re(window.wx);
-        });
-    }
-  });
-}
+      });
+    })
+    .then((res) => {
+      return res;
+    });
